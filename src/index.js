b45ff9cb6e4bb1430fc8c1e9c0d231f64322a95b
Mona Lisa <Mona Lisa>
2024-12-27 15:33:37 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
-const { Telegraf, Markup } = require("telegraf");
+const {
+	Telegraf,
+	Markup
+} = require("telegraf");
 
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
 });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -28,61 +31,64 @@ if (process.env.WEBHOOK === "") {
 }
 
 bot.start(async (ctx) => {
-  try {
-      const message_data = await ctx.reply(
-        "Привет! Чтобы начать использовать бота, откройте WebApp, нажав на кнопку ниже.",
-        {
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "Cargo",
-                  url: process.env.WEB_APP_CARGO, // Укажите URL вашего WebApp
-                },
-              ],
-            ],
-          },
-        }
-      );
-  } catch (err) { 
-    console.error("Ошибка при запуске:", err);
-    await ctx.reply("Произошла ошибка при запуске." + err);
-  }
+	try {
+		const message_data = await ctx.reply(
+			"Привет! Чтобы начать использовать бота, откройте WebApp, нажав на кнопку ниже.", {
+				reply_markup: {
+					inline_keyboard: [
+						[{
+							text: "Cargo",
+							url: process.env.WEB_APP_CARGO, // Укажите URL вашего WebApp
+						}, ],
+					],
+				},
+				disable_web_page_preview: true, // Отключение превью ссылки
+			}
+		);
+
+	} catch (err) {
+		console.error("Ошибка при запуске:", err);
+		await ctx.reply("Произошла ошибка при запуске." + err);
+	}
 });
 
 bot.action(/delete_(.+)/, async (ctx) => {
-  try {
-    const callbackData = ctx.match[1]; // Получаем данные из группы (.+)
-    const message = ctx.callbackQuery.message; // Сообщение, связанное с callback
-    const [formType, messageId] = callbackData.split("_");
+	try {
+		const callbackData = ctx.match[1]; // Получаем данные из группы (.+)
+		const message = ctx.callbackQuery.message; // Сообщение, связанное с callback
+		const [formType, messageId] = callbackData.split("_");
 
-    if (formType === "cargo") {
-      await bot.telegram.editMessageText(
-        "@cargo_life", // Либо ID канала
-        messageId, // ID сообщения
-        undefined, // inlineMessageId, если он не используется
-        `<s>${message.text}</s>\n\n<b>⭕️ Объявление снято с публикации</b>`,
-        {
-          parse_mode: "HTML",
-        } // Новый текст сообщения
-      );
+		if (formType === "cargo") {
+			await bot.telegram.editMessageText(
+				"@cargo_life", // Либо ID канала
+				messageId, // ID сообщения
+				undefined, // inlineMessageId, если он не используется
+				`<s>${message.text}</s>\n\n<b>⭕️ Объявление снято с публикации</b>`, {
+					parse_mode: "HTML", // Указывает форматирование текста
+					disable_web_page_preview: true, // Отключение превью ссылки
+				}
+			);
 
-      await ctx.editMessageText(
-        `<s>${message.text}</s>\n\n<b>⭕️ Объявление снято с публикации</b>`,
-        {
-          parse_mode: "HTML",
-        }
-      );
-    }
-  } catch (err) {
-    console.error("Ошибка при удалении записи:", err);
-    await ctx.reply("Произошла ошибка при удалении записи.");
-  }
+
+			await ctx.editMessageText(
+				`<s>${message.text}</s>\n\n<b>⭕️ Объявление снято с публикации</b>`, {
+					parse_mode: "HTML",
+					disable_web_page_preview: true, // Отключение превью ссылки
+				}
+			);
+		}
+	} catch (err) {
+		console.error("Ошибка при удалении записи:", err);
+		await ctx.reply("Произошла ошибка при удалении записи.");
+	}
 });
 
 app.post("/api/sendMessage", async (req, res) => {
 	try {
-	  const message = `
+		let message, channel, message_data;
+		
+		if (req.body.data.form === "cargo") {
+			message = `
 	  📦 Отправка посылки
 	  📱 Груз: ${req.body.data.type}
 	  ⚖️ Вес: ${req.body.data.weight}
@@ -91,51 +97,59 @@ app.post("/api/sendMessage", async (req, res) => {
 	  📍 Куда: ${req.body.data.to}
 	  ${req.body.data.comment ? `📝 Комментарий: ${req.body.data.comment}` : ""}
 		  `;
-  
-	  let message_data;
-	  if (req.body.data.form === "cargo") {
-		message_data = await bot.telegram.sendMessage(
-		  "@cargo_life", // ID канала
-		  message,
-		  Markup.inlineKeyboard([
-			Markup.button.url(
-			  "Написать автору",
-			  `https://t.me/${req.body.user.username}`
-			),
-		  ])
-		);
-	  } else if (req.body.data.form === "exchange") {
-	  //   message_data = await bot.telegram.sendMessage(
-	  //     "", // ID канала
-	  //     message
-	  //     // Markup.inlineKeyboard([
-	  //     //   Markup.button.callback("🗑 Удалить", `delete_${savedCargo._id}`),
-	  //     // ])
-	  //   );
-	  }
-	  if (message_data) {
-		await bot.telegram.sendMessage(
-		  req.body.user.chatId, // ID канала
-		  message,
-		  Markup.inlineKeyboard([
-			Markup.button.callback(
-			  "🛑 Снять с публикации",
-			  `delete_${req.body.data.form}_${message_data.message_id}`
-			),
-		  ])
-		);
-	  }
-  
-	  res.send(message);
+		  
+			channel = 'cargo_life'
+			message_data = await bot.telegram.sendMessage(
+				`@${channel}`, // ID канала
+				message, {
+					...Markup.inlineKeyboard([
+						Markup.button.url(
+							"Написать автору",
+							`https://t.me/${req.body.user.username}`
+						),
+					]),
+					disable_web_page_preview: true, // Отключение превью ссылки
+				}
+			);
+		} else if (req.body.data.form === "exchange_saudi") {
+			channel = 'exchange_saudi'
+			//   message_data = await bot.telegram.sendMessage(
+			//     "", // ID канала
+			//     message
+			//     // Markup.inlineKeyboard([
+			//     //   Markup.button.callback("🗑 Удалить", `delete_${savedCargo._id}`),
+			//     // ])
+			//   );
+		}
+		if (message_data && channel) {
+			await bot.telegram.sendMessage(
+				req.body.user.chatId, // ID пользователя
+				message, {
+					...Markup.inlineKeyboard([
+						Markup.button.callback(
+							"🛑 Снять с публикации",
+							`delete_${req.body.data.form}_${message_data.message_id}`
+						),
+						Markup.button.url(
+							"Посмотреть объявление",
+							`https://t.me/${channel}/${message_data.message_id}`
+						),
+					]),
+					disable_web_page_preview: true, // Отключение превью ссылки
+				}
+			);
+		}
+
+		res.send(message);
 	} catch (error) {
-	  res.status(400).json({
-		message: "Ошибка при создании записи",
-		error: err.message,
-	  });
+		res.status(400).json({
+			message: "Ошибка при создании записи",
+			error: err.message,
+		});
 	}
-  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+	console.log(`listening on port ${PORT}`);
 });
